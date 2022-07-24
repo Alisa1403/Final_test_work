6ff34b8633ffc52f75e96053840b42fb499303c9
Alisa <Alica1200@gmail.com>
2022-07-24 21:44:21 +0300

    Add main solution

--- Program.cs
+++ Program.cs
@@ -6,3 +6,19 @@
     }
     Console.WriteLine();
 }
+
+string[] inputArray = { "hsju", "eee", "ngv668lh", "776ff@@", "1829j", "$$", "@$$$$$$", "qwf", "uhgfdfsf", "b7tbb2", "^" };
+string[] outputArray = new string[inputArray.Length];
+int n =0;
+int size = 4;
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length < size)
+    {   
+        outputArray[n] = inputArray[i];
+        n++;
+    }
+}
+
+Print(outputArray);
